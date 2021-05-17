5962fe30ab5579fc5e9794abf15c580896b07337
Gabriel Nicol <moraesnicol@gmail.com>
2021-05-17 01:25:47 -0300

    mocks added

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,44 @@ void main(List<String> arguments) async {
 
   print('Connected to Postgres database...');
 
-  await conn.query('''
-  CREATE TABLE customers(
-    id serial primary key not null
-    name text,
-    email text,
-    address text,
-    country text,
-  )
-  ''');
+//  await conn.query('''
+//  CREATE TABLE customers(
+//    id serial primary key not null,
+//    name text,
+//    email text,
+//    address text,
+//    country text
+//)
+  // ''');
+
+//TODO Create data
+//  await conn.query('''
+//  INSERT INTO customers  (name, email, address, country)
+//  VALUES ('Gabriel Nicol', 'moraesnicol@gmail.com', 'rua narnia 123', 'Vacinolândia')
+//  ''');
+
+//TODO Read data
+
+  var results = await conn.query('SELECT * from customers');
+  //print(results);
+
+  //for (var row in results) {
+  //   print('''
+  //   ===
+  //   id: ${row[0]}
+  //   name: ${row[1]}
+  //   email: ${row[2]}
+  //   address: ${row[3]}
+  //   country: ${row[4]}
+  //   ===
+  //   ''');
+  // }
+
+  //TODO  Update data
+  // await conn.query("UPDATE customers SET country='Ghana' WHERE id=1");
+  //TODO  Delete data
+
+  await conn.query('DELETE FROM customers WHERE id > 0');
 
   await conn.close();
 }
